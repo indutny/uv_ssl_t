b43325a8c45a4908f8cc926d1510a69e8878b43b
Fedor Indutny <fedor@indutny.com>
2016-05-27 22:05:53 -0400

    example: simplify

--- example/src/main.c
+++ example/src/main.c
@@ -26,6 +26,9 @@
 /* Declaration of `middle_methods` */
 #include "middle.h"
 
+#define CHECK(V) if ((V) != 0) abort()
+#define CHECK_ALLOC(V) if ((V) == NULL) abort()
+
 typedef struct client_s client_t;
 
 static uv_tcp_t server;
@@ -68,53 +71,35 @@ static void connection_cb(uv_stream_t* s, int status) {
   int err;
   client_t* client;
 
-  client = malloc(sizeof(*client));
-  assert(client != NULL);
-
-  client->ssl = SSL_new(ctx);
-  assert(client->ssl != NULL);
+  CHECK_ALLOC(client = malloc(sizeof(*client)));
+  CHECK_ALLOC(client->ssl = SSL_new(ctx));
 
   SSL_set_accept_state(client->ssl);
 
-  err = uv_tcp_init(uv_default_loop(), &client->tcp);
-  assert(err == 0);
+  CHECK(uv_tcp_init(uv_default_loop(), &client->tcp));
+  CHECK(uv_accept(s, (uv_stream_t*) &client->tcp));
+  CHECK(uv_link_source_init(&client->source, (uv_stream_t*) &client->tcp));
 
-  err = uv_accept(s, (uv_stream_t*) &client->tcp);
-  assert(err == 0);
+  CHECK_ALLOC(client->ssl_link =
+      uv_ssl_create(uv_default_loop(), client->ssl, &err));
+  CHECK(err);
 
-  err = uv_link_source_init(&client->source, (uv_stream_t*) &client->tcp);
-  assert(err == 0);
-
-  client->ssl_link = uv_ssl_create(uv_default_loop(), client->ssl, &err);
-  assert(client->ssl_link != NULL && err == 0);
-
-  err = uv_link_chain(&client->source.link, uv_ssl_get_link(client->ssl_link));
-  assert(err == 0);
-
-  err = uv_link_init(&client->middle, &middle_methods);
-  assert(err == 0);
-
-  err = uv_link_chain(uv_ssl_get_link(client->ssl_link), &client->middle);
-  assert(err == 0);
-
-  err = uv_link_observer_init(&client->observer);
-  assert(err == 0);
-
-  err = uv_link_chain(&client->middle, &client->observer.link);
-  assert(err == 0);
+  CHECK(uv_link_chain(&client->source.link, uv_ssl_get_link(client->ssl_link)));
+  CHECK(uv_link_init(&client->middle, &middle_methods));
+  CHECK(uv_link_chain(uv_ssl_get_link(client->ssl_link), &client->middle));
+  CHECK(uv_link_observer_init(&client->observer));
+  CHECK(uv_link_chain(&client->middle, &client->observer.link));
 
   client->observer.read_cb = read_cb;
   client->observer.link.data = client;
 
-  err = uv_link_read_start(&client->observer.link);
-  assert(err == 0);
+  CHECK(uv_link_read_start(&client->observer.link));
 }
 
 
 int main() {
   static const int kBacklog = 128;
 
-  int err;
   uv_loop_t* loop;
   struct sockaddr_in addr;
 
@@ -125,29 +110,21 @@ int main() {
   ERR_load_crypto_strings();
 
   /* Initialize SSL_CTX */
-  ctx = SSL_CTX_new(SSLv23_method());
-  assert(ctx != NULL);
+  CHECK_ALLOC(ctx = SSL_CTX_new(SSLv23_method()));
 
   SSL_CTX_use_certificate_file(ctx, "test/keys/cert.pem", SSL_FILETYPE_PEM);
   SSL_CTX_use_PrivateKey_file(ctx, "test/keys/key.pem", SSL_FILETYPE_PEM);
 
   loop = uv_default_loop();
 
-  err = uv_tcp_init(loop, &server);
-  assert(err == 0);
-
-  err = uv_ip4_addr("0.0.0.0", 9000, &addr);
-  assert(err == 0);
-
-  err = uv_tcp_bind(&server, (struct sockaddr*) &addr, 0);
-  assert(err == 0);
+  CHECK(uv_tcp_init(loop, &server));
+  CHECK(uv_ip4_addr("0.0.0.0", 9000, &addr));
+  CHECK(uv_tcp_bind(&server, (struct sockaddr*) &addr, 0));
 
   fprintf(stderr, "Listening on 0.0.0.0:9000\n");
 
-  err = uv_listen((uv_stream_t*) &server, kBacklog, connection_cb);
-
-  err = uv_run(loop, UV_RUN_DEFAULT);
-  assert(err == 0);
+  CHECK(uv_listen((uv_stream_t*) &server, kBacklog, connection_cb));
+  CHECK(uv_run(loop, UV_RUN_DEFAULT));
 
   return 0;
 }
